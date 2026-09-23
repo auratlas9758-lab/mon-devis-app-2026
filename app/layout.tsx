21f9afb729acf1d885492e6fa68fa6fdcb508550
auratlas9758-lab <auratlas9758@gmail.com>
2026-09-23 09:21:54 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
@@ -31,7 +32,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+        {children}
+        <Analytics />
+      </body>
+      </html>
+    );
+  }
     </html>
   );
 }
